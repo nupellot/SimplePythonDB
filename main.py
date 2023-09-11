52d4a8f85afd7b15bb063a6444b6de55cbb6e7b6
nupellot <54524404+nupellot@users.noreply.github.com>
2023-09-11 15:27:30 +0000

    Доделал минимально рабочую ООП-версию

--- main.py
+++ main.py
@@ -1,99 +1,93 @@
+class Database:
+    def __init__(self):
+        self.data = {}  # Словарь для хранения данных.
+        self.transactions = []  # Стек для хранения изменений в транзакциях.
 
-# Функция для вывода текущего состояния бд.
-def print_variables(variables, transaction_depth):
-    print("\033[44m", "Текущее состояние базы", "\033[0m", end=" ")
-    if transaction_depth != 0:
-        print(f"({transaction_depth} вложенность транзакции)")
-    else:
-        print("")
-    for name in variables:
-        print("\033[45m", name, "\033[0m", sep=" ", end=" ")
-        print("\033[0m", variables[name], "\033[0m", sep="")
-
-
-# Главная цикличная функция для обработки команд пользователя.
-def process_commands(global_variables, changes, transaction_depth):
-    # Вечный цикл ждёт ввода и завершается при получении команды "END"
-    while True:
-        line = input(">" * (transaction_depth + 1) + " ")
-        # Обарабыватем исключения.
-        if line == "END":  # Штатно завершаем работу программы.
-            print("Завершение работы программы")
-            raise SystemExit
-        arguments = line.split()  # Разбиваем ввод на аргументы.
-        if len(arguments) < 1:
-            print("Недостаточно аргументов")
-            continue
-        if len(arguments) > 3:
-            print("Слишком много аргументов")
-            continue
-
-        
-        # Обрабатываем команды пользователя.
-        
-        variables = global_variables | changes
-        # Текущее локальное состояние переменных, с которым мы работаем.
-        
-        command = arguments[0]
-        # Обработка команд, не изменяющих состояние БД.
-        if command == "GET":
-            print(variables.get(arguments[1]))
-        elif command == "FIND":
-            for key in variables:
-                if variables[key] == arguments[1]:
-                    print(key)
-        elif command == "COUNTS":
-            counter = 0
-            for key in variables:
-                if variables[key] == arguments[1]:
-                    counter += 1
-            print(counter)
-        # Обработка команд, изменяющих состояние БД.
-        elif command == "SET":
-            if len(arguments) == 3:
-                changes[arguments[1]] = arguments[2]
-            else:
-                print("Неподходящие аргументы. Верный формат: /SET <ИМЯ> <ЗНАЧЕНИЕ>")
-        elif command == "UNSET":
-            changes[arguments[1]] = "NULL"
-
-        # Обрабатываем команды, связанные с транзакциями.
-        elif command == "BEGIN":
-            changes |= process_commands(global_variables, 
-                                        changes.copy(), 
-                                        transaction_depth + 1)
-            for change in dict(changes):
-                if changes[change] == "NULL":
-                    changes.pop(change)
-        elif command == "COMMIT":
-            if transaction_depth != 0:
-                return changes
-            else:
-                print("Нет транзакции, которую можно было бы завершить")
-                continue
-                
-        elif command == "ROLLBACK":
-            if transaction_depth != 0:
-                return {}
-            else:
-                print("Нет транзакции, которую можно было бы завершить")
-                continue
-        # Транзакции реализованы через рекурсивный вызов главной функции.
-        # Каждый вызов в стеке хранит ссылку на исходное состояние БД
-        # а также свою копию всех совершенных в транзакции изменений.
-
-        
-        elif command == "HELP":  # В случае, если пользователь не угадал с командой.
-            with open('help.txt', 'r') as file:
-                print(file.read())
+    def set(self, key, value):
+        # Записываем изменение в текущую транзакцию либо в основное хранилище.
+        if self.transactions:
+            self.transactions[-1][key] = value
         else:
-            print(f"Неизвестная команда \"{command}\". Используйте HELP")
+            self.data[key] = value
 
-        # Для удобства выводим текущее состояние БД после каждой команды.
-        print_variables(global_variables | changes, transaction_depth)
+    def get(self, key):
+        return self.data.get(key, "NULL")
+
+    def unset(self, key):
+        # Записываем изменение в текущую транзакцию
+        if self.transactions:
+            self.transactions[-1][key] = None
+        elif key in self.data:
+            del self.data[key]
+
+    def count(self, value):
+        count = list(self.data.values()).count(value)
+        return count
+
+    def find(self, value):
+        found_keys = [key for key, val in self.data.items() if val == value]
+        return found_keys
+
+    def begin(self):
+        self.transactions.append({})
+
+    def rollback(self):
+        if self.transactions:
+            self.transactions.pop()
+
+    def commit(self):
+        if self.transactions:
+            current_transaction = self.transactions.pop()
+            for key, value in current_transaction.items():
+                if value is None:
+                    if key in self.data:
+                        del self.data[key]
+                else:
+                    self.data[key] = value
 
 
-# Хранилище переменных.
-global_variables = {}
-# Запуск программы.
-process_commands(global_variables, {}, 0)
+# Главная функция приложения
+def main():
+    db = Database()
+
+    while True:
+        try:
+            command = input("> ").split()
+            if not command:
+                continue
+            cmd = command[0]
+
+            if cmd == "SET":
+                key, value = command[1], command[2]
+                db.set(key, value)
+            elif cmd == "GET":
+                key = command[1]
+                print(db.get(key))
+            elif cmd == "UNSET":
+                key = command[1]
+                db.unset(key)
+            elif cmd == "COUNTS":
+                value = command[1]
+                print(db.count(value))
+            elif cmd == "FIND":
+                value = command[1]
+                keys = db.find(value)
+                print(" ".join(keys))
+            elif cmd == "BEGIN":
+                db.begin()
+            elif cmd == "ROLLBACK":
+                db.rollback()
+            elif cmd == "COMMIT":
+                db.commit()
+            elif cmd == "END":
+                break
+            else:
+                print("Неверная команда")
+
+        except EOFError:
+            print("Приложение завершено.")
+            break
+
+
+if __name__ == "__main__":
+    main()
